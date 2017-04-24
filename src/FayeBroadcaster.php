0a3c9d2132f72ec8f80da15cceda38ad587977d5
Arnis Lielturks <arnis.lielturks@gmail.com>
2017-04-24 16:50:27 +0300

    initial code

---
+++ src/FayeBroadcaster.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace ArnisLielturks\FayeBroadcaster;
+
+use Illuminate\Support\Arr;
+use Illuminate\Support\Str;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use ArnisLielturks\FayeService;
+use Illuminate\Broadcasting\Broadcasters\Broadcaster;
+
+class FayeBroadcaster extends Broadcaster
+{
+    /**
+     * The Redis instance.
+     *
+     * @var \Illuminate\Contracts\Redis\Factory
+     */
+    protected $faye;
+
+    /**
+     * Create a new broadcaster instance.
+     *
+     * @param  \Illuminate\Contracts\Redis\Factory  $redis
+     * @param  string  $connection
+     * @return void
+     */
+    public function __construct($config)
+    {
+        $this->faye = new FayeService($config);
+    }
+
+    /**
+     * Authenticate the incoming request for a given channel.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return mixed
+     */
+    public function auth($request)
+    {
+        return true;
+        if (Str::startsWith($request->channel_name, ['private-', 'presence-']) &&
+            ! $request->user()) {
+            throw new HttpException(403);
+        }
+
+        $channelName = Str::startsWith($request->channel_name, 'private-')
+            ? Str::replaceFirst('private-', '', $request->channel_name)
+            : Str::replaceFirst('presence-', '', $request->channel_name);
+
+        return parent::verifyUserCanAccessChannel(
+            $request, $channelName
+        );
+    }
+
+    /**
+     * Return the valid authentication response.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $result
+     * @return mixed
+     */
+    public function validAuthenticationResponse($request, $result)
+    {
+        if (is_bool($result)) {
+            return json_encode($result);
+        }
+
+        return json_encode(['channel_data' => [
+            'user_id' => $request->user()->getAuthIdentifier(),
+            'user_info' => $result,
+        ]]);
+    }
+
+    /**
+     * Broadcast the given event.
+     *
+     * @param  array  $channels
+     * @param  string  $event
+     * @param  array  $payload
+     * @return void
+     */
+    public function broadcast(array $channels, $event, array $payload = [])
+    {
+        $payload['event_object'] = $event;
+        foreach ($channels as $channel) {
+            $this->faye->send($channel, $payload, []);
+        }
+    }
+}
---
+++ src/FayeBroadcasterProvider.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace ArnisLielturks\FayeBroadcaster;
+
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Broadcast;
+use Illuminate\Broadcasting\BroadcastManager;
+use App\Services\FayeBroadcaster\FayeBroadcaster;
+use Log;
+
+class BroadcastServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot(BroadcastManager $broadcastManager)
+    {
+        $broadcastManager->extend('faye', function ($app, array $config) {
+            return new FayeBroadcaster(config('faye'));
+        });
+    }
+}
